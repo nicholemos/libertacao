d773cc69f7904d62c1b5377de64cbbc9e8d0b315
Nicholas Lemos <40637239+nicholemos@users.noreply.github.com>
2025-01-10 15:24:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("A página foi carregada com sucesso!");
     // Você pode adicionar mais funcionalidades aqui
 });
+
+
+// Função para mostrar e esconder a descrição
+function toggleDescription() {
+    var description = document.querySelector('.item-description');
+    description.style.display = (description.style.display === 'block') ? 'none' : 'block';
+}
+
+
