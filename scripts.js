8f18562779903a57f0bbccd0251cb2f78b7d8093
Nicholas Lemos <40637239+nicholemos@users.noreply.github.com>
2025-01-10 15:15:11 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Função de inicialização ou qualquer outro código adicional que você queira adicionar
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("A página foi carregada com sucesso!");
+    // Você pode adicionar mais funcionalidades aqui
+});
