10520c5d3b1368d7d10e7e48dab2f4fcc11459f5
Nicholas Lemos <40637239+nicholemos@users.noreply.github.com>
2025-01-20 16:44:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,127 @@
-// Lista de solicitantes com seus detalhes
-const solicitantes = [
+ // Lista de solicitantes com descrição e seus estabelecimentos
+       const solicitantes = [
     { 
         nome: "Antiquário",
         descricao: "Esse pequeno chalé de madeira tem um ar misterioso e simpático, com grandes janelas de vidro pontilhado colorido e um pergolado coberto por rosas minúsculas na entrada. Seu interior exibe um salão apinhado de estantes contendo todo tipo de item, mágico ou não. Durante o dia é possível encontrar Ashtad, sentada na sacada, geralmente lendo; e durante a noite Khordad cuida do lugar, iluminado à luz de velas.",
         servicos: "Compra e venda de acessórios mágicos. Sempre que um personagem procura um acessório específico, role sua disponibilidade: 75% para acessórios menores, 50% para acessórios médios e 25% para acessórios maiores.",
         honrarias: "Você recebe um acessório mágico menor (caso seja um personagem de patamar veterano), médio (campeão) ou maior (lenda) à sua escolha.",
-        imagem: "https://static.wikia.nocookie.net/legendofthecryptids/images/5/5a/Magic_Shop_Owner_Graciel.png"
+        imagem: "https://i.pinimg.com/originals/43/5d/8d/435d8df737a6c400b2f0710d37d3b5fb.gif"
     },
     { 
         nome: "Arena", 
-        descricao: "Este anfiteatro é um lugar para combatentes testarem seus talentos e ganharem algum dinheiro. A grande campeã, Caliandre, está sempre pronta para lutar de forma amistosa, trazendo à tona as habilidades dos personagens. Combates contra criaturas ou viajantes planares às vezes acontecem na arena.",
-        servicos: "Desafios e lutas amigáveis entre combatentes, além de alguns combates únicos.",
-        honrarias: "Vencedores podem ganhar reputação ou uma premiação em dinheiro.",
-        imagem: "https://pm1.aminoapps.com/6756/0428b43f1e88de03fda43cbf4d1684c0e6990a2ev2_hq.jpg"
+	descricao: "Este anfiteatro é um lugar para combatentes testarem seus talentos e ganharem algum dinheiro. A grande campeã, Caliandre, está sempre pronta para lutar de forma amistosa, trazendo à tona as habilidades dos personagens. Também é possível encontrar aqui o minotauro Juno, antigo gladiador de Tapista; e Martin, que em vida era um mercenário em Portsmouth. Os combates reúnem espectadores de Candeh’ssa, ansiosos para ver os heróis lutando, em confrontos (quase) nunca mortais. Combates contra criaturas ou viajantes planares às vezes acontecem na arena."
+	servicos: "Uma vez por repouso em Candeh’ssa, cada personagem pode participar de um combate na arena e fazer uma aposta em tibares. Seu desempenho em combate é representado com um teste de Luta:
+ • 9 ou menos. Perde toda a quantia apostada.
+ • 10 a 14. Recebe metade da quantia apostada.
+ • 15 a 19. Recebe toda a quantia apostada.
+ • 20 a 29. Recebe o dobro da quantia apostada.
+ • 30 ou mais. Recebe o triplo da quantia apostada.
+ Um personagem também pode apostar em uma luta como expectador, sem participar dela. Neste caso, use Jogatina.",
+        honrarias: "Você recebe um poder de combate à sua escolha",
+        imagem: "https://static.wikia.nocookie.net/mashle/images/2/2f/Sounds_Movement_Spell_%28Anime%29.gif"
+    },
+
     },
     { 
         nome: "Biblioteca", 
-        descricao: "Essa construção alta tem arquitetura de linhas retas e elegantes, com colunas ornamentais em mármore branco, e cercada de grandes espelhos d’água. Fica próxima ao antiquário e ao bosque, sendo destino recorrente de Eh’mmed, o Sábio.",
-        servicos: "Consulta de livros e pergaminhos sobre magia, história e ciências antigas.",
-        honrarias: "Acesso a conhecimento oculto ou mágico, dependendo do status do visitante.",
-        imagem: "https://cdna.artstation.com/p/assets/images/images/006/532/382/large/javier-lazo-library.jpg"
+	descricao: "Essa construção alta tem arquitetura de linhas retas e elegantes, com colunas ornamentais em mármore branco, e cercada de grandes espelhos d’água. Fica próxima ao antiquário e ao bosque, sendo destino recorrente de Eh’mmed, o Sábio; Teclis, o escriba; e Barossa, a Estudiosa. É normal encontrar qualquer deles nos corredores silenciosos da biblioteca, muitas vezes surpreendendo visitantes curiosos — afinal, não são necessariamente “recepcionistas” do lugar."
+	servicos: "Meios para testes de Conhecimento e outras perícias próprias para encontrar informações sobre qualquer assunto. Também vende pergaminhos. Para comprar um pergaminho com uma magia específica, role 1d6. Se o resultado é maior que o círculo do pergaminho desejado, ele está disponível."
+	honrarias:"Quando você faz um teste de Conhecimento ou Misticismo, pode pagar 1 PM para rolar dois dados e escolher o melhor resultado.",
+	imagem:"https://i.imgflip.com/9hc3ud.gif"
+
+,
+    },
+    { 
+        nome: "Bosque", 
+	descricao: "Não muito distante da cidade, após caminhada breve, pode-se chegar a um belo bosque lilás. As altas copas das árvores dividem espaço com pequenos arbustos, em uma vegetação diversa que se mistura. Luz do dia atravessa a folhagem, desenhando sombras no solo forrado de folhas secas. Nenhum vozerio urbano chega até aqui, apenas os sons da natureza: vento nas folhas, canto de pássaros, zumbido de insetos, algum animal maior passando pela mata. Cheiro de terra molhada se mescla ao amadeirado das árvores e perfume das rosas onipresentes. Frutas silvestres e cogumelos também são abundantes. Em meio a tanta beleza e paz, pode-se ver Lassan-ce, a protetora do bosque, andando livremente. Além dela, há outra moradora: a tímida Viridriane."
+	serviços:" Clareiras para treinos e meditação, território de caça ou coleta de materiais e ingredientes encontrados nos ermos.",
+	honrarias:"Você pode usar a habilidade Marca da Presa como um caçador de mesmo nível (Tormenta20, p. 50). Se já tem esta habilidade, cada dado de dano dela aumenta em um passo (de +2d10 para +2d12 por exemplo)",
+	imagem":"https://64.media.tumblr.com/5ae76b1b0de4c5c910bb17d753efbe95/tumblr_p5lt0qX26B1uhx88zo1_500.gif"
+    },
+    { 
+        nome: "Casa de Banho", 
+	descricao: "Esta ampla construção de pedra poderia parecer imponente e agressiva, mas curiosamente não é; seus vapores e cheiros adocicados são como um leve afago aos que passam em frente. Ao entrar, são recebidos por Aaliyah, a concierge do lugar. Dentro, belas pinturas de paisagens relaxantes revestem as paredes, levando até salas de banho azulejadas em azul muito claro e limpo. Pequenos banquinhos de madeira se espalham no aposento aberto, ao lado de baldes e buchas vegetais. Lavar-se aqui é obrigatório, antes de chegar ao propósito verdadeiro do lugar. Após a limpeza corporal, é permitido adentrar o paraíso: vapor delicioso eleva-se da água quente, tornando o lugar aconchegante. Sob inebriante cheiro de almíscar e lavanda, a sensação é de estar no céu. Como se estas águas termais pudessem levar todo o cansaço, todos os problemas, para bem longe."
+	serviços:" Seus PV máximos aumentam em +5 por patamar. Uma noite aqui também remove qualquer condição temporária.",
+	honrarias:" Uma vez por dia, você obtém o resultado máximo nos dados de cura de uma de suas habilidades de cura.",
+	imagem":"https://media.tenor.com/kDhmlgCEhoYAAAAM/kimetsu-no-yaiba-demon-slayer.gif"
+    },
+    { 
+        nome: "Churrascaria", 
+	descricao: "Fumaça com cheiro de carne exala constante da chaminé ao lado deste grande prédio. Em seu vasto salão circular, muitas mesas e cadeiras cercando a bancada de um grande cozinheiro — literal e figurativamente falando. Tonhão, o churrasqueiro, segue virando espetos e salgando carnes, enquanto Punddin, a gourmand, prepara pratos principais e acompanhamentos. Quem acredita que comida não é arte, nunca viu esses dois trabalhando juntos."
+	serviços:"Bebidas e pratos comuns sem custo, 
+quaisquer outras por seu custo normal.
+ Também pode-se preparar o famoso misturadinho dos deuses, prato exclusivo feito com insumos fornecidos pelos heróis. Cada porção pode ter até três 
+acompanhamentos diferentes, exigindo um ingrediente ou um mantimento (veja Ameaças de Arton, p. 401) por acompanhamento. Os efeitos dos acompanhamentos 
+são cumulativos e contam como um único bônus de alimentação.
+	 • Linguiça Desconstruída. Feita com uma mistura de carnes bem preparadas, orgulho da casa e sempre disponível. Fornece +2d4 PV temporários.
+	 • Espetinho de Kraken. Esta carne salgada pelas águas é dura e borrachenta, mas compensa com um sabor exótico. Fornece +1d4 PM temporários. 
+	• Melancia Brilhante. Famosa opção vegetariana, cultivada com a mais pura magia natural e irrigada com auxílio de boguns recém-invocados e frescos. Fornece +1 em testes de Resistência.
+	 • Vinagrete Glópico. Salada cortada em cubos com uma pitada de tempero cítrico, deliciosa, leve e sempre presente em toda mesa de churrasco. Aumenta o deslocamento em +1,5m.
+	 • Arroz. Acompanhamento simples e certeiro, harmoniza com tudo. Fornece +2 em uma perícia a sua escolha (exceto Luta e Pontaria).
+Uma porção de misturadinho com quaisquer acompanhamentos custa T$ 50.
+",
+	honrarias:"Você recebe +2 em Fortitude e, uma vez por dia, pode remover uma de suas condições de cansaço",
+	imagem":"https://i0.wp.com/beneaththetangles.com/wp-content/uploads/2022/09/uwp826573.gif"
+    },
+    { 
+        nome: "Estalagem", 
+	descricao: "Esta construção térrea está entre as mais chamativas para recém-chegados a Candeh’ssa, com seu interior todo revestido de madeira escura e vitrais coloridos. Apesar da imponência, é um lugar tranquilo e quase sempre vazio, ainda que muito bem cuidado por Mahin. Oferece todo tipo de acomodação para diferentes hóspedes com diferentes necessidades, incluindo tanques de água salgada e um grande jardim interno."
+	serviços:" Seus PV máximos aumentam em +5 por patamar. Inclui vários quartos especiais para sereias, elfos-do-mar, devotos de Allihanna…",
+	honrarias:"Seu descanso recupera 1 PM e 1 PV adicional por nível.",
+	imagem":""
+    },
+    { 
+        nome: "Guilda", 
+	descricao: "Salão comunal com dois andares, a guilda está sempre agitada. Cidadãos de Candeh’ssa requisitam e cumprem missões postadas no grande quadro atrás do balcão, onde a amistosa Myssia, a recepcionista, os gerencia. Longas mesas com assentos acomodam grupos de aventureiros para planejar novas incursões ou celebrar aquelas bem-sucedidas."
+	serviços:"A guilda oferece missões requisitadas por outros NPCs. A natureza de cada missão e sua recompensa podem ser escolhidas pelo mestre, ou 
+resolvidas aleatoriamente usando a tabela na página 34.",
+	honrarias:"Você recebe uma recompensa adicional em cada missão. Role 1d6 novamente na coluna Recompensa da Tabela 0-3: Missões Aleatórias.",
+	imagem":"https://i.pinimg.com/originals/a3/af/42/a3af429c423aafafea02e923dc3f7ca7.gif"
+    },
+    { 
+        nome: "Laboratório", 
+	descricao: "O interior deste casebre, abarrotado de frascos com ingredientes alquímicos e maquinários estranhos, é também o lar de Nthanda e Frann. Suas chaminés exalam incessante fumaça colorida, e ocasionalmente é possível ouvir explosões em seu interior."
+	serviços:"Itens alquímicos. Também  vende poções mágicas. Para comprar uma poção de uma 1d6. Se o resultado é maior que o círculo da poção desejada, ela está disponível.",
+	honrarias:"A CD dos itens alquímicos que você usa aumenta em +2; ou você recebe uma engenhoca que simula uma magia de círculo que pode criar, e que não conta em seu limite de engenhocas.",
+	imagem":"https://i.gifer.com/9BaN.gif"
+    },
+    { 
+        nome: "Mercado", 
+descricao: "Antes mesmo de chegar à praça principal, pode-se ouvir o burburinho. Bazares se espalham de forma aparentemente caótica, mas o olhar atento logo percebe certa organização. Mercadores anunciam suas ofertas, clientes negociam pechinchas. Vocês logo percebem estar no coração comercial de Candeh’ssa: o Mercado. Aqui se encontra todo tipo de mercadoria — assim como todo tipo de gente. Uma enorme banca se sobressai, oferecendo quinquilharias estranhamente ordenadas, apesar da grande quantidade. Esta tenda pertence a Tharuk, o mercador, e sua filha Nahiri. Também chama atenção uma banca menor, porém cheirosa e vistosa: pertence a Lyssara, a florista."
+	serviços:"Todos os itens comuns (não mágicos) presentes no livro básico Tormenta20, não encontrados nos outros locais de Candeh’ssa (bem como quaisquer outras coisas a critério do mestre).",
+	honrarias:"Uma vez por repouso, você recebe 30% de desconto no preço de um único item.",
+	imagem":"https://i.pinimg.com/originals/31/c6/30/31c63081c66f0fadd715a9d73da6a134.gif"
+    },
+    { 
+        nome: "Oficina", 
+	descricao: "Instalada em uma casa de pedra com um amplo pátio, onde é possível avistar uma forja e diversas bancadas de trabalho, a Oficina de Candeh’ssa é também morada dos três mestres artífices da cidade: Harko, o armeiro; Phylla, a couraceira; e Kandor, o artesão. Quando um deles trabalha, é normal reunir pequenos bandos de admiradores; mesmo após séculos, não cansam de testemunhar o trabalho destes grandes mestres."
+	serviços:" Armas, escudos e armaduras normais e superiores e aplicação de melhorias em itens destas categorias. Também é possível comprar versões mágicas destes itens ou solicitar a aplicação de encantos. Para isso, role a disponibilidade do item (veja o quadro na p. 29). Chance de 75% para itens menores, 50% para itens médios e 25% para itens maiores. Para a disponibilidade de encantos, considere o nível que o item terá se o encanto for aplicado.",
+	honrarias:"Um item do personagem recebe uma melhoria à sua escolha (exceto material especial), cujos pré-requisitos cumpra.",
+	imagem":"https://static.wikia.nocookie.net/powerlisting/images/5/55/Oetsu_Nimaiya_forging_skills.gif"
+    },
+    { 
+        nome: "Pesqueiro", 
+	descricao: "Próxima ao melhor ponto de pesca no lago central de Candeh’ssa, uma casa modesta oferece equipamentos e iscas para aqueles buscando um desafio repousante. Um pequeno cais contém alguns botes a remo para aluguel (sem custo para os aventureiros). Não muito longe, sistemas de redes formam criadouros de peixes para manter o lago sempre abastecido. Nerus, a pescadora, e seu atual marido, Andris, são encontrados com frequência nos arredores."
+	serviços:" O Pesqueiro oferece descanso e… peixes. Um personagem que escolha ficar aqui faz um teste de Sobrevivência (CD 15). Se passar, consegue 
+um pescado (peixe, crustáceo, molusco etc.). Para cada 10 que excede a CD, um pescado adicional. Pescados podem ser oferecidos a Andris para serem tratados, rendendo 1 recurso natural (Ameaças de Arton, p. 401) do tipo mantimento ou osso por pescado. Um personagem também pode vender cada pescado por 
+2d10 x 10 T$ cada.", 
+	honrarias:" Uma arma à sua escolha recebe a melhoria lanajuste (Ameaças de Arton, p. 400), cumulativa com outros materiais especiais. Se já tem esta melhoria, a arma recebe +1 em sua margem de ameaça",
+	imagem":"https://66.media.tumblr.com/c08c5dff642f175e9e5f164e45445fa6/tumblr_ppy8hoTgl61vk2qmmo1_500.gif"
+    },
+    { 
+        nome: "Taverna", 
+	descricao: "Nesta cidade de repouso para aventureiros, os deuses sabem que não poderia faltar uma boa taverna! Aberta e bem frequentada todas as horas do dia, oferece um salão aconchegante com grandes mesas, pinturas festivas e o perene perfume de boa comida preparada por Nima, a taverneira. Existe um palco para sediar apresentações de bardos e dançarinas (diz- -se que certa elfa celestial, arquimaga épica quando ainda viva, faz apresentações ocasionais), também disponível para aventureiros. Oferece também mesas de jogo gerenciadas pela crupiê Artemisia. A taverna é vigiada pelo sempre atento Bragi, que desencoraja qualquer briga (isto é, exceto quando os aventureiros se esforçam para começar uma)."
+	serviços:" Quaisquer itens da categoria Alimentação. Sempre cheia, a taverna é também lugar propício para testes de Jogatina nas mesas de jogo e Investigação sobre as masmorras ou a própria cidade. Personagens treinados em Atuação podem se apresentar no palco para obter alguns tibares.
+",
+	honrarias:"Você recebe +2 em testes de perícias baseadas em Carisma e, uma vez por aventura, pode fazer um teste de Diplomacia para mudar atitude como ação completa sem sofrer a penalidade padrão de –10.",
+	imagem":"https://theanimeharvest.wordpress.com/wp-content/uploads/2016/04/konosuba-puking.gif"
+    },
+    { 
+        nome: "Templo", 
+	descricao: "No centro de Candeh’ssa, este estabelecimento se destaca por sua imponência. Vinte colunas se erguem ao redor, dedicadas a cada deus do Panteão, trazendo cada uma a efígie de seu patrono no topo. Suas pilastras afiladas fazem a estrutura inteira lembrar uma majestosa coroa, enquanto o interior é adorado com mosaicos e vitrais mágicos. O templo é cuidado por muitos espíritos e gênios, que respondem às duas autoridades locais: Rafiq, o Piedoso, que ministra ritos dedicados aos deuses de energia positiva, e Tanit, a Oculta, celebrante dedicada aos deuses de energia negativa."
+	serviços:" Ritos de penitência para devotos que tenham violado Obrigações & Restrições de suas divindades. Também oferece água benta, itens esotéricos que afetam magias divinas e serviços de magias divinas. Devotos de deuses que canalizam energia positiva recebem 10% de desconto em qualquer transação.",
+	honrarias:" Caso seja devoto, você recebe um poder concedido extra de sua divindade.",
+	imagem":"https://pa1.aminoapps.com/6698/c05c82e7f98a99cbf8151f265f39dc645e11de46_00.gif"
     }
 ];
 
